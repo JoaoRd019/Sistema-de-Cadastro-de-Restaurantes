7726b1fdc939f605b60c291c14f8fedf9ce5e16f
JoaoRd019 <joao_kassis@hotmail.com>
2025-12-27 11:33:27 -0300

    initial commit: add main application structure for restaurant management

---
+++ app.py
@@ -0,0 +1,77 @@
+import os
+
+restaurantes = []
+
+
+
+def exibir_nome_do_programa():
+    print(""""
+██████████████████████████████████████████████████████████████████████████
+█─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█
+█▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█
+▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀\n""")
+    
+
+def exibir_opcoes():
+    print("""1.Cadastrar Restaurante
+2.Listar Restaurante
+3.Ativar Restaurante
+4.Sair\n""")
+
+def finalizar_app():
+    os.system("cls")
+    print("finalizando app\n")
+
+def opcao_invalida():
+    print("Opção invalida\n")
+    input("Digite uma tecla para voltar ao menu principal")
+    main()
+
+def cadastrar_novo_restaurante():
+    os.system("cls")
+    print("Cadastro de novos restaurantes\n")
+    nome_do_restaurante = input("Digite o nome do restaurante que deseja cadstrar: ")
+    restaurantes.append(nome_do_restaurante)
+    print(f"O restaurante {nome_do_restaurante} foi cadastrado com sucesso")
+    input("Digite uma tecla para voltar ao menu: ")
+    main()
+
+def listar_restaurantes():
+    os.system("cls")
+    print("Listando restaurantes\n")
+    
+    for i in restaurantes:
+         print(f".{i}")
+
+    input("\nDigite uma tecla para voltar ao menu: ")
+    main()
+
+def ativar_restaurantes():
+     pass
+
+     
+def escolher_opcao():
+    try:  
+        opcao_escolhida = int(input("Escolha uma opçao: "))
+
+        if opcao_escolhida == 1:
+                cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+                listar_restaurantes()
+        elif opcao_escolhida == 3:
+                ativar_restaurantes()
+        elif opcao_escolhida == 4:
+                finalizar_app()
+        else:
+            opcao_invalida()
+    except ValueError:
+         opcao_invalida()
+           
+def main():
+    os.system("cls")
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == "__main__":
+    main()
